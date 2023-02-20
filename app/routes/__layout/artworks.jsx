99242fad1eddd0d4c3d648c64098002394c2fef5
fweth <klausgy@gmail.com>
2023-02-20 11:36:24 +0000

    no prefetch on artworks

--- app/routes/__layout/artworks.jsx
+++ app/routes/__layout/artworks.jsx
@@ -52,7 +52,6 @@ export default function Artworks() {
           className={tn.doubleCol ? "double" : "single"}
           to={`${tn.artwork.slug}/1`}
           key={tn.artwork.slug}
-          prefetch="intent"
         >
           <figure>
             <Image
